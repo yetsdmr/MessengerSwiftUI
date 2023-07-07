15f5dae1fc8a999764f587ed0d831bfc84257f18
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-07-07 15:47:51 +0300

    Initial Commit

---
+++ MessengerSwiftUI/MessengerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerSwiftUIApp.swift
+//  MessengerSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 7.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
