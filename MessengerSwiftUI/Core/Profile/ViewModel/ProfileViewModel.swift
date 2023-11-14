84a503cee1b3e0a94708112edddcb101275e7db4
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-14 16:09:39 +0300

    Added Profile Photo Selection.

---
+++ MessengerSwiftUI/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewModel.swift
+//  MessengerSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 14.11.2023.
+//
+
+import SwiftUI
+import PhotosUI
+
+class ProfileViewModel: ObservableObject {
+    @Published var selectedItem: PhotosPickerItem? {
+        didSet {
+            Task { 
+                try await loadImage ()
+            }
+        }
+    }
+    
+    @Published var profileImage: Image?
+    
+    func loadImage() async throws {
+        guard let item = selectedItem else { return }
+        guard let imageData = try await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: imageData) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
