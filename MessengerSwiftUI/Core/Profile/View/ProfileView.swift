6639a1dcf78b0a6f36f50aeb7936fb9f26f8ec7c
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-14 16:01:29 +0300

    Added Profile View UI.

---
+++ MessengerSwiftUI/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileView.swift
+//  MessengerSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 14.11.2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            // header
+            VStack {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(Color(.systemGray4))
+                
+                Text("Bruce Wayne")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+            }
+            
+            // list
+            List {
+                Section {
+                    ForEach(SettingsOptionsViewModel.allCases) { option in
+                        HStack {
+                            Image(systemName: option.imageName)
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundColor(option.imageBackgroundColor)
+                            
+                            Text(option.title)
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                
+                Section {
+                    Button("Log Out") {
+                        
+                    }
+                    
+                    Button("Delete Account") {
+                        
+                    }
+                }
+                .foregroundStyle(.red)
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
