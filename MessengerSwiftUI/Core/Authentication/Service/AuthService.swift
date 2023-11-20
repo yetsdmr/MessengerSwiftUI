df535d7b93890b8d3b28fc9d67b3fc7e9203df01
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-20 16:26:15 +0300

    Fetching Users for Chats.

--- MessengerSwiftUI/Core/Authentication/Service/AuthService.swift
+++ MessengerSwiftUI/Core/Authentication/Service/AuthService.swift
@@ -17,7 +17,7 @@ class AuthService {
     
     init() {
         self.userSession = Auth.auth().currentUser
-        Task { try await UserService.shared.fetchCurrentUser() }
+        loadCurrentUserData()
         print ("DEBUG: User session id is \(userSession?.uid)")
     }
     
@@ -26,6 +26,7 @@ class AuthService {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
             self.userSession = result.user
+            loadCurrentUserData()
         } catch {
             print ("DEBUG: Failed to sign in user with error: \(error.localizedDescription)")
         }
@@ -37,6 +38,7 @@ class AuthService {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
             try await self.uploadUserData(email: email, fullname: fullname, id: result.user.uid)
+            loadCurrentUserData()
         } catch {
             print ("DEBUG: Failed to create user with error: \(error.localizedDescription)")
         }
@@ -46,6 +48,7 @@ class AuthService {
         do {
             try Auth.auth().signOut() // signs out on backend
             self.userSession = nil // updates routing logic
+            UserService.shared.$currentUser = nil
         } catch {
             print("DEBUG: Failed to sign out with error \(error.localizedDescription)")
         }
@@ -55,5 +58,9 @@ class AuthService {
         let user = User(fullname: fullname, email: email, profileImageUrl: nil)
         guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
         try await Firestore.firestore().collection("users").document(id).setData(encodedUser)
+    }
+    
+    private func loadCurrentUserData() {
+        Task { try await UserService.shared.fetchCurrentUser() }
     }
 }
--- MessengerSwiftUI/Core/NewMessage/View/NewMessageView.swift
+++ MessengerSwiftUI/Core/NewMessage/View/NewMessageView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct NewMessageView: View {
     @State private var searchText = ""
+    @StateObject private var viewModel = NewMessageViewModel()
     @Environment (\.dismiss) var dismiss
     
     var body: some View {
@@ -25,13 +26,13 @@ struct NewMessageView: View {
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding()
                 
-                ForEach(0 ... 10, id:\.self) { user in
+                ForEach(viewModel.users) { user in
                     VStack {
                      
                         HStack {
-                            CircularProfileImageView(user: User.MOCK_USER, size: .xSmall)
+                            CircularProfileImageView(user: user, size: .xSmall)
                             
-                            Text("Chadwick Bozeman")
+                            Text(user.fullname)
                                 .font(.subheadline)
                                 .fontWeight(.semibold)
                             
