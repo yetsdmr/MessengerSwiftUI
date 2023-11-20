662e7ed03946271be96f23d5f9ee6d6345a37a8e
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-20 12:52:20 +0300

    Added Upload/Download User Data.

--- MessengerSwiftUI/Core/Authentication/Service/AuthService.swift
+++ MessengerSwiftUI/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 class AuthService {
     
@@ -16,9 +17,11 @@ class AuthService {
     
     init() {
         self.userSession = Auth.auth().currentUser
+        Task { try await UserService.shared.fetchCurrentUser() }
         print ("DEBUG: User session id is \(userSession?.uid)")
     }
     
+    @MainActor
     func login(withEmail email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -28,10 +31,12 @@ class AuthService {
         }
     }
     
+    @MainActor
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            try await self.uploadUserData(email: email, fullname: fullname, id: result.user.uid)
         } catch {
             print ("DEBUG: Failed to create user with error: \(error.localizedDescription)")
         }
@@ -46,5 +51,9 @@ class AuthService {
         }
     }
     
-    
+    private func uploadUserData(email: String, fullname: String, id: String) async throws {
+        let user = User(fullname: fullname, email: email, profileImageUrl: nil)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try await Firestore.firestore().collection("users").document(id).setData(encodedUser)
+    }
 }
---
+++ MessengerSwiftUI/Service/UserService.swift
@@ -0,0 +1,24 @@
+//
+//  UserService.swift
+//  MessengerSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 20.11.2023.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+class UserService {
+    @Published var currentUser: User?
+    
+    static let shared = UserService()
+    
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        let user = try snapshot.data(as: User.self)
+        self.currentUser = user
+        print("DEBUG: Current user in service is \(currentUser)")
+    }
+}
